b64243e45b9844e332077eeeaedd77d9ea4192c4
mostua <mostua@gmail.com>
2016-12-11 12:52:53 +0100

    Sprawdzanie konstruktora Cell

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
-import Lib
-import Cell
+import           Cell
+import           Lib
 
 
 main :: IO ()
-main = someFunc
+main = putStrLn $ show $  Cell (CellCord 1 2) (SUMFunc (RangeParam (CellCord 1 2) (CellCord 1 3))) "SUM(A2:A3)"
