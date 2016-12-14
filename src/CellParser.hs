0cc0b8a8e848fac4bad6a006e7be057c316c7f3a
mostua <mostua@gmail.com>
2016-12-15 00:30:29 +0100

    Parser komórek

--- src/CellParser.hs
+++ src/CellParser.hs
@@ -1,5 +1,5 @@
 module CellParser
-    ( parseCell, pFuncName, pOneLetterColumn, pTwoLetterColumn, parseCellCord
+    ( parseCell, pFuncName, parseCellCord, columnToNumber
     ) where
 
 import Cell
@@ -17,50 +17,39 @@ pFuncName :: GenParser Char st FuncName
 pFuncName = SUMFunc <$ string "sum"
             <|> MULFunc <$ string "mul"
             <|> AVGFunc <$ string "avg"
-            <?> "no function found"
+            <?> "function name"
 
 numOfChar :: Char -> Int
 numOfChar x = ord x - ord 'a' + 1
 
+digitValue :: Char -> Int
+digitValue x = ord x - ord '0'
+
 alphSize :: Int
-alphSize = ord 'z' - ord 'a'
+alphSize = ord 'z' - ord 'a' + 1
 
-pOneLetterColumn :: GenParser Char st Int
-pOneLetterColumn = do 
-                    a <- lower 
-                    return $ numOfChar a
 
-pTwoLetterColumn :: GenParser Char st Int
-pTwoLetterColumn = do 
-                    a <- lower 
-                    b <- lower 
-                    return (numOfChar a * alphSize + numOfChar b)
+columnToNumber :: String -> Int
+columnToNumber letters = foldl (\x y -> x * alphSize + numOfChar y) 0 letters 
 
--- | parse a non-negative number given a base and a parser for the digits
-number :: (Integral i, Stream s m t) => Int -> ParsecT s u m Char
-  -> ParsecT s u m i
-number base baseDigit = do
-  n <- liftM (numberValue base) (many1 baseDigit)
-  seq n (return n)
-
--- | compute the value from a string of digits using a base
-numberValue :: Integral i => Int -> String -> i
-numberValue base =
-  foldl (\ x -> ((fromIntegral base * x) +) . fromIntegral . digitToInt) 0
+digitsToNumber :: String -> Int
+digitsToNumber digits = foldl (\x y -> x * 10 + digitValue y) 0 digits
 
 
 parseCellCord :: GenParser Char st CellCord
 parseCellCord = do
-                  a <- try pTwoLetterColumn <|> pOneLetterColumn
-                  b <- number 10 digit
-                  return (CellCord a b)
+                  a <- many1 lower <?> "column letter"
+                  b <- many1 digit <?> "row number"
+                  let column = columnToNumber a
+                  let row = digitsToNumber b
+                  return (CellCord column row)
 
 
 {-
 
 cell = func_cell | num_cell | string_cell 
 func_cell = '=' func_name ( params* )
-position = ('A-Z' | 'A-Z''A-Z') [0-9]* 
+position = [A-Z]* [0-9]* 
 range = position ':' position
 param = position | range
 params = params (';' params)*
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,7 +5,7 @@ import CellParser
 import Data.Either
 import Text.Parsec.String
 
-testFuncParsed :: Parsec String () FuncName -> String -> FuncName -> Test
+testFuncParsed :: (Eq a) => Parsec String () a -> String -> a -> Test
 testFuncParsed parser toParse expectedValue = let parseResult = parse parser "" toParse
                                               in TestCase (assertBool "isRight" (isRight parseResult && (rights [parseResult] == [expectedValue]))) 
 
@@ -14,9 +14,11 @@ testFuncNotParsed parser toParse = let parseResult = parse parser "" toParse
                                    in TestCase (assertBool "isLeft" (isLeft parseResult))
 
 tests = TestList [TestLabel "sumFunc" (testFuncParsed pFuncName "sum" SUMFunc)
-                 ,TestLabel  "avgFunc" (testFuncParsed pFuncName "mul" MULFunc)
+                 ,TestLabel "avgFunc" (testFuncParsed pFuncName "mul" MULFunc)
                  ,TestLabel "mulFunc" (testFuncParsed pFuncName "avg" AVGFunc)
-                 ,TestLabel "mulFunc" (testFuncNotParsed pFuncName "adam")]
+                 ,TestLabel "failTest" (testFuncNotParsed pFuncName "adam")
+                 ,TestLabel "parseCellA1" (testFuncParsed parseCellCord "a1" (CellCord 1 1))
+                 ,TestLabel "parseCellZ10" (testFuncParsed parseCellCord "z10" (CellCord 26 10))]
 
 main :: IO Counts
 main = do runTestTT tests
