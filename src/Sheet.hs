e15b53e743a72836476adf84637a4b0abe9e22f6
Adam Mościcki <mostua@gmail.com>
2016-12-19 13:25:54 +0100

    Parsowanie arkusza

--- src/Sheet.hs
+++ src/Sheet.hs
@@ -2,11 +2,14 @@ module Sheet (
     Sheet (Sheet),
     Dim (Dim),
     getWidth, getHeight,
-    createEmptySheet
+    createEmptySheet,
+    readShet,
+    parseSheet
 ) where
 
 import           Cell
-import           Data.Array
+import           CellParser
+--import           Data.Array
 
 -- width height
 data Dim = Dim Int Int deriving (Show, Eq)
@@ -21,15 +24,42 @@ instance Measurable Dim where
     getWidth (Dim width _) = width
     getHeight (Dim _ height) = height
 
+
+type SheetContent = [[Cell]]
+
 -- arkuszt składa się z
-data Sheet = Sheet Dim (Array Int (Array Int Cell)) deriving (Show, Eq)
+data Sheet = Sheet Dim SheetContent deriving (Show, Eq)
 
 instance Measurable Sheet where
     getWidth (Sheet x _) = getWidth x
     getHeight (Sheet x _) = getHeight x
 
+-- tworzy pusty arkusz
 createEmptySheet :: Int -> Int -> Sheet
-createEmptySheet width height = let createRow column h = array (1,h) [(row, Cell (CellCord column row) (StringCell "") "") | row <- [1..h]]
-                                    createArray w h = array (1, w) [(column, createRow column h) | column <- [1..w]]
-                                in Sheet (createDim width height) (createArray width height)
+createEmptySheet width height = let createRow rowNum = [Cell (CellCord rowNum columnNum) (StringCell "") "" | columnNum <- [1..height]]
+                                    array = [createRow rowNum | rowNum <- [1..width]]
+                                in Sheet (createDim width height) array
 
+sheetListToContent :: [[CellContent]] -> [[String]] -> Int -> Int -> Sheet
+sheetListToContent parsedCells originCells width height
+                  = let getCell tab x y = (tab !! (x - 1)) !! (y - 1)
+                        createRow rowNum = [Cell (CellCord rowNum columnNum)
+                                                 (getCell parsedCells columnNum rowNum)
+                                                 (getCell originCells columnNum rowNum) | columnNum <- [1..height]]
+                        array = [createRow rowNum | rowNum <- [1..width]]
+                    in Sheet (createDim width height) array
+
+-- zamienia [[]] na listę [[CellContent]]
+parseSheet :: [[String]] -> [[CellContent]]
+parseSheet = map (map parseCell)
+
+
+
+-- wczytuje arkusz z tablicy stringów
+-- -> wiersz []
+-- -> kolumna [[]]
+readShet :: [[String]] -> Sheet
+readShet cells = let height = length cells
+                     width | height == 0 = 0
+                           | otherwise = length $ head cells
+                 in createEmptySheet width height
